0c2e4f5d80ed818e9d14286a983749889303da62
Chris Travers <chris.travers@gmail.com>
2017-11-15 10:10:44 +0100

    Changes for 0.0,3

---
+++ country--0.0.2--0.0.3.sql
@@ -0,0 +1,24 @@
+
+DO $$
+DECLARE version_num integer;
+BEGIN
+  SELECT current_setting('server_version_num') INTO STRICT version_num;
+  IF version_num > 90600 THEN
+	EXECUTE $E$ ALTER FUNCTION country_in(cstring) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_out(country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_recv(internal) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_send(country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country(text) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION text(country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_eq(country, country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_ne(country, country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_lt(country, country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_le(country, country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_gt(country, country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_ge(country, country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION country_cmp(country, country) PARALLEL SAFE $E$;
+	EXECUTE $E$ ALTER FUNCTION hash_country(country) PARALLEL SAFE $E$;
+  END IF;
+END;
+$$;
+
