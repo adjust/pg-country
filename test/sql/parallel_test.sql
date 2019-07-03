cbc6a626a75fe21d93859959c4e496c204f6253c
Ildar Musin <zildermann@gmail.com>
2019-07-03 15:54:22 +0200

    Adapt tests for PostgreSQL 11 & 12

--- test/sql/parallel_test.sql
+++ test/sql/parallel_test.sql
@@ -2,6 +2,7 @@ BEGIN;
 create extension country;
 SET max_parallel_workers_per_gather=4;
 SET force_parallel_mode=on;
+SET enable_gathermerge=off;
 
 CREATE TABLE parallel_test(i int, c country) WITH (parallel_workers = 4);
 INSERT INTO parallel_test (i, c)
@@ -12,9 +13,9 @@ LATERAL (SELECT CASE WHEN i % 3 = 0 THEN 'US'::country
                      WHEN i % 3 = 2 THEN 'CA'::country
          END as country) c;
 
-EXPLAIN (costs off,verbose)
+EXPLAIN (costs off, verbose)
 SELECT COUNT(*) FROM parallel_test WHERE  c = 'us';;
 
-EXPLAIN (costs off,verbose)
+EXPLAIN (costs off, verbose)
 SELECT c, COUNT(*) FROM parallel_test GROUP BY 1;
 ROLLBACK;
