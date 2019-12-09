f38f90ae0ae5262fb9ab8c0f259776cc3dbe0062
Oleksii Kliukin <alexk@hintbits.com>
2019-12-09 12:48:41 +0100

    Minor code cleanup

--- src/country.c
+++ src/country.c
@@ -6,8 +6,8 @@ PG_MODULE_MAGIC;
 static inline char *
 create_string(const char *chars, size_t sz)
 {
-    char *str = palloc0((sz) * sizeof(char));
-    memcpy(str, chars, (sz) * sizeof(char));
+    char *str = palloc0((sz));
+    memcpy(str, chars, (sz));
     return str;
 }
 
