57d7a6fb46c2cfa5f68157473014694387832dc6
Ildar Musin <zildermann@gmail.com>
2021-10-07 22:41:18 +0200

    Build docker image to run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,28 +1,10 @@
 #!/usr/bin/env bash
 
-set -ux
+set -eux
 
-# global exports
-export PGPORT=55435
+cd /repo
+pg-init
+build-check
 
-# build extension
-make install
-
-# initialize database
-initdb -D $PGDATA
-
-# start cluster
-pg_ctl start -l /tmp/postgres.log -w -o "-p $PGPORT"
-
-# something's wrong, exit now!
-[[ $? -ne 0 ]] && cat /tmp/postgres.log && exit 1;
-
-# run regression tests
-export PG_REGRESS_DIFF_OPTS="-w -U3" # for alpine's diff (BusyBox)
-make installcheck
-
-# show diff if needed and exit if something's wrong
-[[ $? -ne 0 ]] && { [[ -f regression.diffs ]] && cat regression.diffs ; exit 1 ; }
-
-set +ux
+set +eux
 
